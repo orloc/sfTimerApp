bc010a3ccf5a90c5926cea77eb3651c7c4cbf9c4
Grant Tepper <grant.tepper@gmail.com>
2017-07-27 01:51:57 -0700

    adding stylings and updating settings and user managment

---
+++ app/scripts/directives/groupPermissionForm.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('sfTimer')
+    .directive('deleteGroupForm', ['dataProvider', 'eventBroadcaster', '$interval',
+    function(dataProvider, eventBroadcaster, $interval){
+    return {
+        templateUrl: 'views/directives/deleteGroupFormTemplate.html',
+        scope: {
+            existingGroup: '='
+        }, 
+        link: function(scope, element, attr){
+            scope.submit= function(){
+                dataProvider.deleteTimerGroup(scope.existingGroup)
+                    .then(function(resp){
+                        eventBroadcaster.broadcast(eventBroadcaster.event.timerGroup.delete, resp);
+                        eventBroadcaster.broadcast(eventBroadcaster.event.form.close);
+                    }, function(err){
+                        scope.formError = err.message;   
+                    });
+            };
+            scope.cancel = function(){
+                eventBroadcaster.broadcast(eventBroadcaster.event.form.close);
+            }
+        }
+    };
+}]);
